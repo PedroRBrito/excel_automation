48d276587cd2566bc828102ab72817a00d7bc4a2
PedroRBrito <pedrorocha1654@gmail.com>
2025-04-28 00:49:32 -0300

    Criação da feature filtro e cálculos

--- README.md
+++ README.md
@@ -1,12 +1,54 @@
 # Comparador de Planilhas em Python (PyQt5 + Pandas)
 
-Aplicação desktop feita com PyQt5 para leitura, comparação e exportação de planilhas Excel (.xlsx). O programa permite carregar dois arquivos, comparar valores com base em uma coluna comum (como "Categoria") e gerar automaticamente a diferença absoluta e percentual entre os dados.
+Aplicação desktop feita com Pandas e PyQt5 para leitura, comparação e exportação de planilhas Excel (.xlsx). O programa permite carregar dois arquivos, comparar valores com base em uma coluna comum (como "Categoria") e gerar automaticamente o cálculo escolhido pelo usuário.
 
-## Funcionalidades:
+## Funcionalidades
 
 - Leitura de duas planilhas Excel.
 - Visualização dos dados com ordenação.
 - Barra de progresso durante o carregamento.
-- Comparação entre valores realizados e previstos.
-- Cálculo automático de diferença absoluta e percentual.
+- Comparação entre dois valores.
+- Cálculo automático dos valores.
 - Exportação do resultado para um novo arquivo Excel.
+
+## Tecnologias Utilizadas
+
+- **PyQt5**: Para a criação da interface gráfica do usuário.
+- **Pandas**: Para o processamento e manipulação de dados em planilhas.
+- **Python**: Linguagem de programação utilizada para o desenvolvimento da aplicação.
+
+## Requisitos
+
+- Python 3.x
+- PyQt5 (`pip install pyqt5`)
+- Pandas (`pip install pandas`)
+
+## Como Rodar o Projeto
+
+1. **Clone o repositório**:
+
+   ```bash
+   git clone https://github.com/seu-usuario/comparador-planilhas.git
+   cd comparador-planilhas
+   ```
+
+2. **Instale as dependências**:
+
+   ```bash
+   pip install -r requirements.txt
+   ```
+
+3. **Execute a aplicação**:
+   ```bash
+   python comparador_planilhas.py
+   ```
+
+## Como Usar
+
+1. Ao abrir a aplicação, você verá duas áreas de visualização de tabelas.
+2. Clique em "Abrir primeiro arquivo" para carregar a primeira planilha Excel.
+3. Clique em "Abrir segundo arquivo" para carregar a segunda planilha Excel.
+4. Escolha as colunas das planilhas para serem usadas no cálculo.
+5. Selecione as operações que deseja realizar (Soma, Multiplicação, Divisão, Média, Diferença e Diferença Percentual).
+6. Clique em "Comparar planilhas" para visualizar a comparação entre os dados das duas planilhas.
+7. Você pode exportar o resultado para um novo arquivo Excel.
--- app.py
+++ app.py
@@ -10,9 +10,10 @@ from PyQt5.QtWidgets import (
     QMessageBox,
     QProgressBar,
     QTableView,
+    QLineEdit,
+    QCheckBox,
 )
-from PyQt5.QtCore import QAbstractTableModel, QObject, Qt
-from click import progressbar
+from PyQt5.QtCore import QAbstractTableModel, Qt
 import pandas as pd
 
 
@@ -32,13 +33,28 @@ class Janela(QWidget):
         self.table_primeiro_arquivo = QTableView()
         self.table_segundo_arquivo = QTableView()
 
+        self.coluna1_input = QLineEdit()
+        self.coluna1_input.setPlaceholderText(
+            "Coluna da primeira planilha para cálculo"
+        )
+
+        self.coluna2_input = QLineEdit()
+        self.coluna2_input.setPlaceholderText("Coluna da segunda planilha para cálculo")
+
         self.progress_bar = QProgressBar()
 
-        self.button1 = QPushButton("Abrir arquivo")
+        self.button1 = QPushButton("Abrir primeiro arquivo")
         self.button1.clicked.connect(self.abrir_arquivo1)
 
         self.button2 = QPushButton("Abrir segundo arquivo")
         self.button2.clicked.connect(self.abrir_arquivo2)
+
+        self.checkbox_soma = QCheckBox("Soma")
+        self.checkbox_multiplicacao = QCheckBox("Multiplicação")
+        self.checkbox_divisao = QCheckBox("Divisão")
+        self.checkbox_media = QCheckBox("Média")
+        self.checkbox_diferenca = QCheckBox("Diferença")
+        self.checkbox_diferenca_percentual = QCheckBox("Diferença Percentual")
 
         self.button_comparar = QPushButton("Comparar planilhas")
         self.button_comparar.clicked.connect(self.comparar_planilhas)
@@ -46,31 +62,42 @@ class Janela(QWidget):
         layout.addLayout(layout_table)
         layout_table.addWidget(self.table_primeiro_arquivo)
         layout_table.addWidget(self.table_segundo_arquivo)
+        layout.addWidget(self.coluna1_input)
+        layout.addWidget(self.coluna2_input)
+        layout.addWidget(self.checkbox_soma)
+        layout.addWidget(self.checkbox_multiplicacao)
+        layout.addWidget(self.checkbox_divisao)
+        layout.addWidget(self.checkbox_media)
+        layout.addWidget(self.checkbox_diferenca)
+        layout.addWidget(self.checkbox_diferenca_percentual)
         layout.addWidget(self.progress_bar)
         layout.addWidget(self.button1)
         layout.addWidget(self.button2)
         layout.addWidget(self.button_comparar)
+        layout.setContentsMargins(10, 10, 10, 10)
+        layout.addSpacing(10)
+
         self.setLayout(layout)
 
     def abrir_arquivo1(self):
+        self.progress_bar.setValue(0)
         self.df1 = self.carregar_excel()
         if self.df1 is not None:
+            self.progress_bar.setValue(50)
             model = PandasModel(self.df1)
             self.table_primeiro_arquivo.setModel(model)
             self.table_primeiro_arquivo.setSortingEnabled(True)
-            QMessageBox.information(
-                self, "Arquivo 1", f"{len(self.df1)} linhas carregadas"
-            )
+            self.progress_bar.setValue(100)
 
     def abrir_arquivo2(self):
+        self.progress_bar.setValue(0)
         self.df2 = self.carregar_excel()
         if self.df2 is not None:
+            self.progress_bar.setValue(50)
             model = PandasModel(self.df2)
             self.table_segundo_arquivo.setModel(model)
             self.table_segundo_arquivo.setSortingEnabled(True)
-            QMessageBox.information(
-                self, "Arquivo 2", f"{len(self.df2)} linhas carregadas!"
-            )
+            self.progress_bar.setValue(100)
 
     def carregar_excel(self):
         filters = "Excel (*.xlsx);;Todos os Arquivos (*)"
@@ -81,10 +108,7 @@ class Janela(QWidget):
             return None
 
         try:
-            self.progress_bar.setValue(0)
-            self.progress_bar.setMaximum(100)
             df = pd.read_excel(file_path)
-            self.progress_bar.setValue(100)
             return df
         except Exception as e:
             QMessageBox.warning(self, "Erro", f"Erro ao carregar: {str(e)}")
@@ -96,21 +120,48 @@ class Janela(QWidget):
                 self, "Erro", "Carregue as duas planilhas antes de comparar."
             )
 
-        df1 = self.df1.rename(columns={"Valor": "Realizado"}) # type: ignore
-        df2 = self.df2.rename(columns={"Valor": "Previsto"}) # type: ignore
+        coluna1 = f"{self.coluna1_input.text()}_1"
+        coluna2 = f"{self.coluna2_input.text()}_2"
 
-        self.comparado = pd.merge(df1, df2, on="categoria", how="outer")
+        self.comparado = pd.merge(
+            self.df1,
+            self.df2,
+            left_on=self.df1.columns[0],
+            right_on=self.df2.columns[0],
+            how="outer",
+            suffixes=("_1", "_2"),
+        )
 
-        self.comparado["Diferença"] = self.comparado["Realizado"] - self.comparado["Previsto"]
-        self.comparado["% Diferença"] = (
-            self.comparado["Diferença"] / self.comparado["Previsto"]
-        ) * 100
+        if self.checkbox_soma.isChecked():
+            self.comparado["Soma"] = self.comparado[coluna1] + self.comparado[coluna2]
+        if self.checkbox_diferenca.isChecked():
+            self.comparado["Diferença"] = (
+                self.comparado[coluna1] - self.comparado[coluna2]
+            )
+        if self.checkbox_multiplicacao.isChecked():
+            self.comparado["Multiplicação"] = (
+                self.comparado[coluna1] * self.comparado[coluna2]
+            )
+        if self.checkbox_divisao.isChecked():
+            self.comparado["Divisão"] = (
+                self.comparado[coluna1] / self.comparado[coluna2]
+            )
+        if self.checkbox_media.isChecked():
+            self.comparado["Média"] = (
+                self.comparado[coluna1] + self.comparado[coluna2]
+            ) / 2
+        if self.checkbox_diferenca_percentual.isChecked():
+            self.comparado["Diferença percentual"] = (
+                (self.comparado[coluna1] - self.comparado[coluna2])
+                / self.comparado[coluna2]
+            ) * 100
 
         self.janela_comparacao = JanelaComparacao(self.comparado)
         self.janela_comparacao.show()
 
     def conectar_exportar(self):
         self.exportar_excel(self.comparado)
+
 
 class JanelaComparacao(QWidget):
     def __init__(self, df):
@@ -140,7 +191,6 @@ class JanelaComparacao(QWidget):
         if file_path:
             self.df.to_excel(file_path, index=False)
             QMessageBox.information(self, "Sucesso", "Arquivo salvo com sucesso.")
-
 
 
 class PandasModel(QAbstractTableModel):
