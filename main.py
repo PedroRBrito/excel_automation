d7c1b6cc338e9a6b49717644e3a23b4aaa8b354c
PedroRBrito <pedrorocha1654@gmail.com>
2025-05-01 03:25:30 -0300

    fix main

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 from PyQt5.QtWidgets import QApplication
 from src.interface import Janela
-import src.style.stylesheet as stylesheet
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
